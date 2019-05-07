f57b1cefd03665aec7b922e59dcf19aa860fd83d
Deep learning by torch <44338800+yahan9416@users.noreply.github.com>
2019-05-07 20:09:18 +0800

    Update oligo_limma.r

--- modules/scripts/oligo_limma.r
+++ modules/scripts/oligo_limma.r
@@ -1,4 +1,4 @@
-required_Packages = c("oligo", "limma","sva")
+required_Packages = c("oligo", "limma","sva","ggplot2","ggrepel","org.Mm.eg.db","org.Hs.eg.db")
 if(!all(required_Packages %in% installed.packages())){
   source("https://bioconductor.org/biocLite.R")
   biocLite(setdiff(required_Packages, installed.packages()))
@@ -8,132 +8,91 @@ require(oligo)
 require(limma)
 require(sva)
 require(ggplot2)
+require(ggrepel)
+require(org.Mm.eg.db)
+require(org.Hs.eg.db)
 
 args = commandArgs(T)
 file_path = args[1] #data file path
 design_path = args[2] #design matrix path
 result_path = args[3] #result path
-gpl_path = args[4]
-FC_setting = args[5] #setting fold change
-pval_setting = args[6] #setting adj.p.val
+cdf_name = args[4] #cdf names
+GPL_path=arg[5]#GPL information path
+FC_setting = args[6] #setting fold change
+pval_setting = args[7] #setting adj.p.val
 FC_setting <- as.numeric(FC_setting)
 pval_setting <- as.numeric(pval_setting)
 logFC_setting <- log2(FC_setting)
+
 dir.create(result_path)
-
-#design_path <- "/Users/shixiaoying/rongbin/gsm.txt"
-#file_path <- "/Users/shixiaoying/rongbin/GSE41867_RAW"
-#gpl_path  <- "/Users/shixiaoying/Downloads/GSE41867_family.soft"
-
-ff <- gpl_path
-nn <- grep("^[^#!^]", readLines(ff))[1] - 1
-tmp <- system(paste0("grep -n '!Sample_organism_ch1' ",ff),intern = TRUE)[1]
-if(gsub(".*= ","",tmp)=="Mus musculus"){
-  db ="org.Mm.eg.db"
-  if(db %in% installed.packages()){
-    library(org.Mm.eg.db)
-  }else{
-    source("https://bioconductor.org/biocLite.R")
-    biocLite("org.Mm.eg.db")
-  }
-}
-if(gsub(".*= ","",tmp)=="Homo sapiens"){
-  db="org.Hs.eg.db"
-  if(db %in% installed.packages()){
-    library(org.Hs.eg.db)
-  }else{
-    source("https://bioconductor.org/biocLite.R")
-    biocLite("org.Hs.eg.db")
-    library(org.Mm.eg.db)
-  }
-}
 
 # Read design
 design_mat = read.table(design_path, sep="\t", header=F)
 gsm = as.vector(design_mat[,1])
-grouplist = as.vector(design_mat[,2])
+grouplist = gsub(" ","",as.vector(unlist(design_mat[,2])))
 
-setwd(file_path)
+
 # read files in and process data
-celFiles <- list.celfiles(path = file_path)
-data.raw <- read.celfiles(filenames = file.path('.', celFiles))
-#pdf("Pre-normalization MA plot",width = 3.5,height = 4)
-#MAplot(data=data.raw)
+celFiles <- list.celfiles(file_path, listGzipped=TRUE,full.names=TRUE)
+data.raw <- read.celfiles(celFiles)
 data.rma <- rma(data.raw)
 data.expr <- exprs(data.rma)
-time_batch <- substr(as.character(protocolData(data.raw)$dates),1,10)
-dates <- as.character(protocolData(data.raw)$dates)
-tmp <- sample(1:50, length(unique(time_batch)), replace=F)
-data.batch <- time_batch
-for(i in 1:length(unique(time_batch))){
-  data.batch[which(time_batch==unique(time_batch)[i])] <- tmp[i]
-}
-# tmp <- sample(1:50, length(unique(grouplist)), replace=F)
-# for(i in 1:length(unique(grouplist))){
-#   grouplist[which(grouplist==unique(grouplist)[i])] <- tmp[i]
-# }
 
-sampleInfo <- data.frame(groupName=colnames(data.expr), date=dates, Condition=grouplist, batchIndex=time_batch)
-{
-  if(length(unique(data.batch)) == 1)
-    data.combat <- data.expr else
-      data.combat <- ComBat(dat = data.expr, batch = as.numeric(data.batch), mod = NULL)
-}
+#Get gene symbol
+gpl_inf=read.table(GPL_path,header=T,sep="\t",fill=T,quote = "")
+symbol=gpl_inf$gene_assignment[match(rownames(data.expr),gpl_inf$ID)]
+symbol=sapply(strsplit(as.vector(unlist(symbol))," //"),function(x) x[1])
+
+if(cdf_name=="org.Mm.eg.db")
+    new_symbol <- select(org.Mm.eg.db, keys=symbol, columns="SYMBOL", keytype="REFSEQ")[,2]
+if(cdf_name=="org.Hs.eg.db")
+    new_symbol <- select(org.Hs.eg.db, keys=symbol, columns="SYMBOL", keytype="REFSEQ")[,2]
+new_symbol=toupper(new_symbol)
+data.expr=aggregate(data.expr,by=list(new_symbol),mean)
+rownames(data.expr)=data.expr[,1]
+data.expr=data.expr[,-1]
+
+#get exprSet  
+#make sure the same sample order in design_matrix and expression profile
+newcolname <- colnames(data.expr)[sapply(gsm,grep,colnames(data.expr),USE.NAMES = F)]
+data.expr <- data.expr[,newcolname]
 colnames(data.expr) <- gsm
-
-#Differential expression analysis
-#convert probe ids to gene symbols
-pfinfo <- read.table(ff, sep = "\t", quote = "", header = TRUE, skip = nn, fill = TRUE)
-ID_idx <- which(colnames(pfinfo)=='ID')
-gene_idx <- which(colnames(pfinfo)=='gene_assignment')
-#gpl <- getGEO(getGEO(gsub("./|_.*",'',celFiles[1]))@header$platform_id)
-#ID_idx <- which(colnames(Table(gpl))=='ID')
-#gene_idx <- which(colnames(Table(gpl))=='gene_assignment')
-#write.csv(Table(gpl)[,c(ID_idx,gene_idx)],"GPL16570.csv")
-ids <- pfinfo[,c(ID_idx,gene_idx)]
-ids_ok <- ids[grepl('^NM', ids$gene_assignment),] #select the lines with gene information
-data.exprs_ann = merge(data.expr, ids_ok, by.x = 0, by.y = 'ID')
-dim(data.exprs_ann)
-data.exprs_ann$gene_assignment <- gsub(' /.*','',data.exprs_ann$gene_assignment)
-data.exprs_ann_rd <- data.exprs_ann[!duplicated(data.exprs_ann$gene_assignment),]
-dim(data.exprs_ann_rd)
-rownames(data.exprs_ann_rd) <- data.exprs_ann_rd$gene_assignment
-data.exprs_ann_rd <- data.exprs_ann_rd[,c(-1,-ncol(data.exprs_ann_rd))]
-data.exprs_ann_rd1 <- data.exprs_ann_rd
-{
-  if(db=="org.Mm.eg.db")
-    data.exprs_ann_rd1$SYMBOL <- select(org.Mm.eg.db, keys=rownames(data.exprs_ann_rd), columns="SYMBOL", keytype="REFSEQ")[,2]
-  if(db=="org.Hs.eg.db")
-    data.exprs_ann_rd1$SYMBOL <- select(org.Hs.eg.db, keys=rownames(data.exprs_ann_rd), columns="SYMBOL", keytype="REFSEQ")[,2]
-}
-data.exprs_ann_rd_sym <- na.omit(data.exprs_ann_rd1[!duplicated(data.exprs_ann_rd1$SYMBOL),])
-rownames(data.exprs_ann_rd_sym) <- data.exprs_ann_rd_sym$SYMBOL
-data.exprs_ann_rd_sym <- data.exprs_ann_rd_sym[,-ncol(data.exprs_ann_rd_sym)]
-write.table(data.exprs_ann_rd_sym, paste0('.','/Full_data.txt'),sep = '\t',quote = F)
-
+write.table(data.expr,paste0(result_path,'/Expression_profile.txt'),sep = '\t',quote = F)
+exprSet <- data.expr[,gsm]
+# do pca analysis
+pca_raw <- prcomp(t(data.expr), center = TRUE, scale. = F)
+edata_pc_df <- as.data.frame(pca_raw$x)
+edata_pc_df$class=grouplist
+pca_plot=ggplot(edata_pc_df, aes(x = PC1, y = PC2, color = class)) +
+    geom_point()
+ggsave(paste0(result_path,"/PCA_plot.png"), pca_plot, width = 5, height = 5)
 
 #do limma
 design <- model.matrix(~0+factor(grouplist))
 colnames(design) = levels(factor(grouplist))
-rownames(design) = colnames(data.exprs_ann_rd_sym)
-fit <- lmFit(data.exprs_ann_rd_sym,design)
-contrast.matrix <- makeContrasts(paste0(rev(unique(grouplist)),collapse = '-'), levels = design)
+rownames(design) = colnames(exprSet)
+fit <- lmFit(exprSet,design)
+condition_matrix=t(combn(unique(grouplist),2))
+cond_matr=matrix((unlist(apply(condition_matrix,1,function(x) paste0(x,collapse = "-")))),nrow=1)
+contrast.matrix <- makeContrasts(contrasts=cond_matr, levels =colnames(fit$coefficients))
 fit <- contrasts.fit(fit, contrast.matrix)
 fit <- eBayes(fit)
-tempoutput <- topTable(fit, number = Inf, lfc = 0, p.value = 1)
-colnames(tempoutput) <- c("log2FoldChange","AveExpr","t","P.Value","padj","B")
-# output file
-write.table(tempoutput,paste0('.','/Different_Expression.txt'),sep = '\t',quote = F)
 
-sigout <- subset(tempoutput, abs(log2FoldChange) > logFC_setting & padj < pval_setting)
+each_condi_DEG<-function(x){
+output <- topTable(fit,coef=x, number = Inf, lfc = 0, p.value = 1)
+colnames(output) <- c("log2FoldChange","AveExpr","t","P.Value","padj","B")
+# output file
+write.table(output,paste0(result_path,'/',x,'Different_Expression.txt'),sep = '\t',quote = F)
+
+sigout <- subset(output, abs(log2FoldChange) > logFC_setting & padj < pval_setting)
 upR <- sigout[which(sigout$log2FoldChange > 0), 0]
 downR <- sigout[which(sigout$log2FoldChange < 0), 0]
-write.table(upR, paste0('.','/Upregulated_gene.txt'),sep = '\t',quote = F,col.names = F)
-write.table(downR, paste0('.','/Downregulated_gene.txt'),sep = '\t',quote = F,col.names = F)
 
-volca <- ggplot(tempoutput,aes(log2FoldChange,-log10(padj))) + 
+volca <- ggplot(output,aes(log2FoldChange,-log10(padj))) + 
   geom_point(aes(color=ifelse((log2FoldChange > logFC_setting | log2FoldChange < -logFC_setting) & padj < pval_setting, "sign","non-sign")), cex = 1, alpha = 0.3) + 
-  scale_colour_manual("Significance", values = c("blue","red")) +
+  scale_colour_manual("Significance", values = c("steelblue","red")) +
   labs(title = 'Volcano plot', x = "log2 Fold Change", y = "-log10 adj.P.Val")+
-  geom_text(data = sigout, aes(x = log2FoldChange, y=-log10(padj), label = rownames(sigout)), cex = 1, vjust = "inward", hjust = "inward")
-ggsave(paste0('.',"/Volcano_plot.png"), volca, width = 7, height = 7)
+  geom_text_repel(data = sigout, aes(label = rownames(sigout)), cex = 3,box.padding = unit(0.35, "lines"), point.padding = unit(0.3, "lines"))
+ggsave(paste0(result_path,'/',x,"Volcano_plot.png"), volca, width = 7, height = 7)
+}
+apply(matrix(colnames(fit$coefficients)),1,each_condi_DEG)
