e0fd6c19e09148ef98396c2fa9e596fa6edfa4af
yahan <yahan@bioinfordeMacBook-Pro.local>
2019-05-16 10:07:35 +0800

    fix bug

--- modules/scripts/draw_volca_plot.r
+++ modules/scripts/draw_volca_plot.r
@@ -28,5 +28,5 @@ volca <- ggplot(file,aes(log2FoldChange,-log10(padj))) +
   geom_point(aes(color=ifelse((log2FoldChange > logFC_setting | log2FoldChange < -logFC_setting) & padj < pval_setting, "sign","non-sign")), cex = 1, alpha = 0.3) + 
   scale_colour_manual("Significance", values = c("steelblue","red")) +
   labs(title = 'Volcano plot', x = "log2 Fold Change", y = "-log10 adj.P.Val")+
-  geom_text_repel(data = sigout, aes(label = rownames(sigout)), cex = 3,box.padding = unit(0.35, "lines"), point.padding = unit(0.3, "lines"))
+  geom_text_repel(data = sigout, aes(label = sigout$SYMBOL), cex = 3,box.padding = unit(0.35, "lines"), point.padding = unit(0.3, "lines"))
 ggsave(result_path, volca, width = 7, height = 7)
