22c9a8977114553fabeeb05a9827fdcdda28734e
yahan <yahan@bioinfordeMacBook-Pro.local>
2019-05-24 13:10:17 +0800

    Modif the scipt of PCA and volcano plot

--- modules/scripts/draw_PCA_plot.r
+++ modules/scripts/draw_PCA_plot.r
@@ -12,6 +12,7 @@ args = commandArgs(T)
 profile_path=args[1] #which is used to save the tpm matrix or expression matrix
 express_type=args[2] # tell the funtion which is RNAseq or Microarray
 design_path=args[3] #describe the subclass of each sample used for PCA
+
 result_path=args[4] #save the figure
 
 expre_matr=as.matrix(read.table(profile_path,header =T,sep="\t"))
@@ -22,10 +23,11 @@ if(express_type == "RNASeq"){
 }
 num_sam=dim(expre_matr)[2]
 expre_matr=matrix(as.numeric(as.vector(unlist(expre_matr))),ncol=num_sam)
+expre_matr=log2(expre_matr)
 pca_raw <- prcomp(t(expre_matr), center = TRUE, scale. = F)
 edata_pc_df <- as.data.frame(pca_raw$x)
 design_matrix=read.table(design_path,header = T,sep=",")
 edata_pc_df$class=design_matrix$treatment
 pca_plot=ggplot(edata_pc_df, aes(x = PC1, y = PC2, color = class)) +
-  geom_point()
-ggsave(result_path, pca_plot, width = 5, height = 5)
+  geom_point()+theme_bw()+theme(axis.text.x = element_text(size = 10))+theme(axis.text.y = element_text(size = 10))
+ggsave(result_path, pca_plot, width = 6, height = 5)
--- modules/scripts/draw_volca_plot.r
+++ modules/scripts/draw_volca_plot.r
@@ -21,12 +21,20 @@ logFC_setting <- log2(FC_setting)
 file=read.table(file_path,header = T,sep="\t")
 
 sigout <- subset(file, abs(log2FoldChange) > logFC_setting & padj < pval_setting)
+index=order(sigout$log2FoldChange)
+if(length(index) > 20){
+sigout=sigout[c(index[1:10],rev(index)[1:10]),]}
+
 upR <- as.vector(unlist(sigout$SYMBOL[which(sigout$log2FoldChange > 0)]))
 downR <- as.vector(unlist(sigout$SYMBOL[which(sigout$log2FoldChange < 0)]))
 
 volca <- ggplot(file,aes(log2FoldChange,-log10(padj))) + 
   geom_point(aes(color=ifelse((log2FoldChange > logFC_setting | log2FoldChange < -logFC_setting) & padj < pval_setting, "sign","non-sign")), cex = 1, alpha = 0.3) + 
-  scale_colour_manual("Significance", values = c("steelblue","red")) +
+  scale_colour_manual("Significance", values = c("grey","red")) +
   labs(title = 'Volcano plot', x = "log2 Fold Change", y = "-log10 adj.P.Val")+
-  geom_text_repel(data = sigout, aes(label = sigout$SYMBOL), cex = 3,box.padding = unit(0.35, "lines"), point.padding = unit(0.3, "lines"))
+  geom_text_repel(data = sigout, aes(label = sigout$SYMBOL), cex = 3,box.padding = unit(0.35, "lines"), point.padding = unit(0.3, "lines"))+
+  theme_bw()+geom_hline(aes(yintercept=-log10(pval_setting)),linetype="dashed",size=0.5)+
+  geom_vline(aes(xintercept=logFC_setting),linetype="dashed",color="grey",size=0.5)+
+  geom_vline(aes(xintercept=-logFC_setting),linetype="dashed",color="grey",size=0.5)+theme(axis.text.x = element_text(size = 10))+theme(axis.text.y = element_text(size = 10))
+
 ggsave(result_path, volca, width = 7, height = 7)
