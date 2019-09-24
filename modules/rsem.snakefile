33fcd6f0a95fb954c17d410710e38bdcbbd7fdfe
Xin Dong <xindong9511@gmail.com>
2019-09-24 00:48:49 -0400

    integrate geo download modules

---
+++ modules/rsem.snakefile
@@ -0,0 +1,154 @@
+#!/usr/bin/env python
+
+#-------------------------------RSEM gene quantification------------------------#
+# @author: Jin Wang @ref:VIPER
+# @email: jwang0611@gmail.com
+# @date: May, 2019
+import pandas as pd
+
+metadata = pd.read_csv(config["metasheet"], index_col=0, sep=',')
+
+def merge_sep_inputs(inputs):
+    inputs_format = ' -f '.join(str(i) for i in list(inputs)[0])
+    return inputs_format   
+
+def rsem_quantification_targets(wildcards):
+    ls = []
+    for sample in config["samples"]:
+        ls.append("analysis/rsem/%s/%s.isoforms.results" % (sample, sample))
+        ls.append("analysis/rsem/%s/%s.genes.results" % (sample, sample))
+    ls.append("analysis/rsem/rsem_tpm_iso_matrix.csv")
+    ls.append("analysis/rsem/rsem_tpm_gene_matrix.csv")
+    return ls
+
+
+rule rsem_quantification_all:
+    input:
+        rsem_quantification_targets
+
+
+rule rsem_quantification:
+    input:
+        "analysis/star/{sample}/{sample}.transcriptome.bam" #just to make sure STAR output is available before STAR_Fusion
+    output:
+        rsem_transcript_out = "analysis/rsem/{sample}/{sample}.isoforms.results",
+        rsem_gene_out = "analysis/rsem/{sample}/{sample}.genes.results"
+    log:
+        "analysis/rsem/{sample}/{sample}.rsem.log"
+    message: 
+        "Running RSEM on {wildcards.sample}"
+    benchmark:
+        "analysis/rsem/{sample}/{sample}.rsem.benchmark.txt"
+    conda: "../envs/rsem_env.yml"
+    params:
+        sample_name = lambda wildcards: "analysis/rsem/{sample}/{sample}".format(sample=wildcards.sample),
+        stranded = "--strand-specific" if config["stranded"] else "",
+        paired_end = "--paired-end" if len(config["mate"]) == 2 else "",
+        gz_support="--star-gzipped-read-file" if config["samples"][metadata.index[0]][0][-3:] == '.gz' else ""
+    threads:
+        2
+    shell:
+        "rsem-calculate-expression -p {threads} {params.stranded} "
+        "{params.paired_end} "
+        "--estimate-rspd "
+        "--bam --no-bam-output "
+        "{input} {config[rsem_index]} "
+        "{params.sample_name} > {log}"
+        # "rsem-calculate-expression -p {threads} {params.stranded} "
+  #       "{params.paired_end} "
+  #       "--star {params.gz_support} "
+  #       "--estimate-rspd --append-names "
+  #       "{input} {config[rsem_index]} "
+  #       "{params.sample_name} > {log}"
+
+rule rsem_iso_matrix:
+    input:
+        rsem_iso_files = expand( "analysis/rsem/{sample}/{sample}.isoforms.results", sample=config["samples"] ),
+        metasheet = config['metasheet']
+    output:
+        rsem_iso_matrix = "analysis/rsem/rsem_tpm_iso_matrix.csv"
+    message: 
+        "Running RSEM matrix generation rule for isoforms"
+    benchmark:
+        "analysis/rsem/rsem_iso_matrix_benchmark.txt"
+    params:
+        args = lambda wildcards, input: merge_sep_inputs({input.rsem_iso_files})
+    conda: "../envs/stat_perl_r.yml"
+    shell:
+        "perl src/raw_and_fpkm_count_matrix.pl --column 5 --metasheet {input.metasheet} --header -f {params.args} 1>{output.rsem_iso_matrix}"
+
+;
+rule rsem_gene_matrix:
+    input:
+        rsem_gene_files = expand( "analysis/rsem/{sample}/{sample}.genes.results", sample=config["samples"] ),
+        metasheet = config["metasheet"]
+    output:
+        rsem_gene_matrix = "analysis/rsem/rsem_tpm_gene_matrix.csv"
+    message: 
+        "Running RSEM matrix generation rule for genes"
+    benchmark:
+        "analysis/rsem/rsem_gene_matrix_benchmark.txt"
+    params:
+        args = lambda wildcards, input: merge_sep_inputs({input.rsem_gene_files})
+    conda: "../envs/stat_perl_r.yml"
+    shell:
+        "perl src/raw_and_fpkm_count_matrix.pl --column 5 --metasheet {input.metasheet} --header -f {params.args} 1>{output.rsem_gene_matrix}"
+
+# rule rsem_filter_gene_ct_matrix:
+#   """filters the rsem gene count.
+#   REPLACES: preprocess.snakefile- filter_Cuff_matrix"""
+#   input:
+#   #TODO: handle batch_effect correction
+#       tpmFile = "analysis/" + config["token"] + "/rsem/rsem_gene_ct_matrix.csv",
+#       annotFile=config['metasheet'],
+#       force_run_upon_config_change = config['config_file']
+#   output:
+#       filtered_tpm = "analysis/" + config["token"] + "/rsem/rsem_gene_ct_matrix.filtered.csv"
+#   params:
+#       sample_names = " ".join(config["ordered_sample_list"])
+#   message: "Generating Pre-processed RSEM TPM matrix file"
+#   benchmark:
+#       "benchmarks/" + config["token"] + "/rsem_filter_gene_ct_matrix.txt"
+#   shell:
+#       "Rscript viper/modules/scripts/rsem_filter_gene_ct_matrix.R "
+#       "--tpm_file {input.tpmFile} "
+#       "--min_samples {config[min_num_samples_expressing_at_threshold]} "
+#       "--TPM_cutoff {config[TPM_threshold]} "
+#       "--filter_miRNA {config[filter_mirna]} "
+#       "--numgenes {config[numgenes_plots]} "
+#       "--out_file {output.filtered_tpm} "
+#       "--sample_names {params.sample_names} "
+
+# rule batch_effect_removal_rsem:
+#   input:
+#   gene_cts = "analysis/" + config["token"] + "/rsem/rsem_gene_ct_matrix.csv",
+#   annotFile = config["metasheet"]
+#   output:
+#   rsemcsvoutput="analysis/" + config["token"] + "/rsem/batch_corrected_rsem_gene_ct_matrix.csv",
+#   rsempdfoutput="analysis/" + config["token"] + "/rsem/rsem_combat_qc.pdf"
+#   params:
+#   batch_column="batch",
+#   datatype = "rsem"
+#   message: "Removing batch effect from Gene Count matrix, if errors, check metasheet for batches, refer to README for specifics"
+#   #priority: 2
+#   benchmark:
+#   "benchmarks/" + config["token"] + "/batch_effect_removal_rsem.txt"
+#   shell:
+#   "Rscript viper/modules/scripts/batch_effect_removal.R {input.gene_cts} {input.annotFile} {params.batch_column} "
+#   "{params.datatype} {output.rsemcsvoutput} {output.rsempdfoutput} "
+#   " && mv {input.gene_cts} analysis/{config[token]}/rsemlinks/without_batch_correction_rsem_gene_ct_matrix.csv "
+
+# rule tpm_plot:
+#   input:
+#   #TODO: handle batch_effect correction
+#   tpm_mat = "analysis/" + config['token'] + "/rsem/rsem_gene_ct_matrix.filtered.csv",
+#   annotFile = config["metasheet"]
+#   output:
+#   tpm_png = "analysis/" + config["token"] + "/plots/gene_counts.tpm.png"
+#   message: "Plot gene counts at various tpm cutoffs"
+#   benchmark:
+#   "benchmarks/" + config["token"] + "/rsem_tpm_plot.txt"
+#   shell:
+#   "Rscript viper/modules/scripts/fpkm_plot.R {input.tpm_mat} {output.tpm_png}"
+
+
--- modules/scripts/geo_rawdown.py
+++ modules/scripts/geo_rawdown.py
@@ -1,111 +1,216 @@
+
+# ================================
+# @auther: Xin Dong
+# @email: xindong9511@gmail.com
+# @date: Sep 2019
+# ================================
+
 import os, sys
-import urllib.request, urllib.error, urllib.parse
+import urllib.request
 import re
 import argparse
 
-# transfer the sra files to fastqfiles and cat the multiple fastq files
-def catfastq(main_path, gsm,srx_infor,srr,lay_type):
-    cmd = ''
+def downloadFastqFromEBI(path,gsm,srr,lay_type):
+    ebi_ftp = "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/"
+    if lay_type == "SINGLE":
+        cat_file = ""
+        if len(srr) == 1:
+            s = srr[0]
+            down_link = ebi_ftp + s[0:6] + "/00" + s[-1] + "/" + s + "/" + s + ".fastq.gz"
+            temp_fq = '%s/%s.fastq.gz'% (path, gsm)
+            os.system('wget %s -O %s \n'%(down_link, temp_fq))
+            if checkFastqSize(temp_fq) != True:
+                return False
+        else:
+            for i in range(len(srr)):
+                down_link = ebi_ftp + srr[i][0:6] + "/00" + srr[i][-1] + "/" + srr[i] + "/" + srr[i] + ".fastq.gz"
+                temp_fq = '%s/%s_temp%s.fastq.gz'% (path, gsm, i+1)
+                os.system('wget %s -O %s \n'%(down_link, temp_fq))
+                if checkFastqSize(temp_fq) != True:
+                    return False
+                os.system('gunzip %s \n'%(temp_fq))
+                cat_file = cat_file + '%s/%s_temp%s.fastq '%(path, gsm,i+1)
+            os.system('cat %s> %s/%s.fastq \n'%(cat_file, path, gsm))
+            os.system('rm %s/%s_temp* \n'%(path, gsm))
+            os.system('gzip %s/%s.fastq \n' % (path, gsm))
+        return True
+    elif lay_type == "PAIRED":
+        cat_file1 = ""
+        cat_file2 = ""
+        if len(srr) == 1:
+            s = srr[0]
+            down_link = ebi_ftp + s[0:6] + "/00" + s[-1] + "/" + s + "/" + s + "_1.fastq.gz"
+            temp_fq = '%s/%s_R1.fastq.gz'% (path, gsm)
+            os.system('wget %s -O %s \n'%(down_link, temp_fq))
+            if checkFastqSize(temp_fq) != True:
+                return False
+            down_link = ebi_ftp + s[0:6] + "/00" + s[-1] + "/" + s + "/" + s + "_2.fastq.gz"
+            temp_fq = '%s/%s_R2.fastq.gz'% (path, gsm)
+            os.system('wget %s -O %s \n'%(down_link, temp_fq))
+            if checkFastqSize(temp_fq) != True:
+                return False
+        else:
+            for i in range(len(srr)):
+                # Read 1
+                down_link = ebi_ftp + srr[i][0:6] + "/00" + srr[i][-1] + "/" + srr[i] + "/" + srr[i] + "_1.fastq.gz"
+                temp_fq = '%s/%s_temp%s_1.fastq.gz'% (path, gsm, i+1)
+                os.system('wget %s -O %s \n'%(down_link, temp_fq))
+                if checkFastqSize(temp_fq) != True:
+                    return False
+                os.system('gunzip %s \n'%(temp_fq))
+                cat_file1 = cat_file1 + '%s/%s_temp%s_1.fastq '%(path, gsm,i+1)
+                # Read 2
+                down_link = ebi_ftp + srr[i][0:6] + "/00" + srr[i][-1] + "/" + srr[i] + "/" + srr[i] + "_2.fastq.gz"
+                temp_fq = '%s/%s_temp%s_2.fastq.gz'% (path, gsm, i+1)
+                os.system('wget %s -O %s \n'%(down_link, temp_fq))
+                if checkFastqSize(temp_fq) != True:
+                    return False
+                os.system('gunzip %s \n'%(temp_fq))
+                cat_file2 = cat_file2 + '%s/%s_temp%s_2.fastq '%(path, gsm,i+1)
+            os.system('cat %s> %s/%s_R1.fastq \n'%(cat_file1, path, gsm))
+            os.system('cat %s> %s/%s_R2.fastq \n'%(cat_file2, path, gsm))
+            os.system('rm %s/%s_temp* \n'%(path, gsm))
+            os.system('gzip %s/%s_R1.fastq \n' % (path, gsm))
+            os.system('gzip %s/%s_R2.fastq \n' % (path, gsm))
+        return True
+    else:
+        return False
+
+def checkFastqSize(fastq):
+    if os.path.getsize(fastq) < 100:
+        os.system("rm %s" % fastq)
+        return False
+    else:
+        return True
+
+def downloadFastqFromGEO(path,gsm,srr,lay_type):
     cat_file1 = ''
     cat_file2 = ''
-    srx_infor = 'ftp://ftp-trace.ncbi.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR'
-    print((lay_type+'\n'+str(len(srr))+' runs'))
+    ncbi_link = 'ftp://ftp-trace.ncbi.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR'
+    sys.stdout.write(str(len(srr))+' runs\n')
     if lay_type == 'SINGLE':
         for i in range(len(srr)):
-            srr[i] = srr[i][1:-12]
-            #ftp = srx_infor + '//' + srr[i] + '/' + srr[i] + '.sra'
-            ftp = srx_infor + '/' + srr[i][:6] + '/' + srr[i] +'/' + srr[i]+ '.sra'
+            ftp = ncbi_link + '/' + srr[i][:6] + '/' + srr[i] +'/' + srr[i]+ '.sra'
             # download the sra files and transmit them into fastq files
-            fsra = '%s/%s_temp%s.sra'%(main_path, gsm, i+1)
-            cmd = cmd + 'wget %s -O %s \n'%(ftp, fsra) 
-            cmd = cmd + 'echo "+++fastq-dump++++" \n\nfastq-dump %s/%s_temp%s.sra -O %s \n'%(main_path, gsm,i+1, main_path) 
-            cat_file1 = cat_file1 + '%s/%s_temp%s.fastq '%(main_path, gsm,i+1) 
-        cmd = cmd + 'cat %s> %s/%s.fastq \n'%(cat_file1,main_path,gsm) 
-        cmd = cmd + 'rm %s/%s_temp* \n'%(main_path, gsm)
-        cmd = cmd + 'gzip %s/%s.fastq \n' % (main_path, gsm)
+            fsra = '%s/%s_temp%s.sra'%(path, gsm, i+1)
+            os.system('wget %s -O %s \n'%(ftp, fsra))
+            if checkFastqSize(temp_fq) != True:
+                return False
+            os.system('echo "+++fastq-dump++++"')
+            os.system('\nfastq-dump %s/%s_temp%s.sra -O %s \n'%(path, gsm,i+1, path))
+            cat_file1 = cat_file1 + '%s/%s_temp%s.fastq '%(path, gsm,i+1) 
+        os.system('cat %s> %s/%s.fastq \n'%(cat_file1,path,gsm))
+        os.system('rm %s/%s_temp* \n'%(path, gsm))
+        os.system('gzip %s/%s.fastq \n' % (path, gsm))
     elif lay_type == 'PAIRED':
         for i in range(len(srr)):
-            srr[i] = srr[i][1:-12]
-            ftp = srx_infor + '/' + srr[i][:6] + '/' + srr[i] +'/' + srr[i]+ '.sra'
-            fsra = '%s/%s_temp%s.sra'%(main_path, gsm, i+1)
-            cmd = cmd + 'wget %s -O %s \n'%(ftp, fsra) 
-            # cmd = cmd + 'python '+os.path.join(main_path+'/modules/checkSRA.py')+' %s\nif [ $? == 1 ];then\necho %s >> %s/SRAchecking_fail.xls\nexit\nfi\n'%(fsra, gsm+','+ftp+','+fsra, recordFolder)
-            cmd = cmd + 'echo "+++fastq-dump++++" \n\nfastq-dump --split-files %s/%s_temp%s.sra -O %s \n'%(main_path, gsm,i+1, main_path) 
-            cat_file1 = cat_file1 + '%s/%s_temp%s_1.fastq '%(main_path, gsm,i+1)
-            cat_file2 = cat_file2 + '%s/%s_temp%s_2.fastq '%(main_path, gsm,i+1)
-        cmd = cmd + 'cat %s> %s/%s_R1.fastq \n'%(cat_file1, main_path,gsm) 
-        cmd = cmd + 'cat %s> %s/%s_R2.fastq \n'%(cat_file2, main_path,gsm) 
-        cmd = cmd + 'rm %s/%s_temp* \n'%(main_path, gsm)
-        cmd = cmd + 'gzip %s/%s_R1.fastq \n'%(main_path, gsm)
-        cmd = cmd + 'gzip %s/%s_R2.fastq \n'%(main_path, gsm)
-    return(cmd)
+            ftp = ncbi_link + '/' + srr[i][:6] + '/' + srr[i] +'/' + srr[i]+ '.sra'
+            fsra = '%s/%s_temp%s.sra'%(path, gsm, i+1)
+            os.system('wget %s -O %s \n'%(ftp, fsra))
+            if checkFastqSize(temp_fq) != True:
+                return False
+            os.system('echo "+++fastq-dump++++" \n')
+            os.system('\nfastq-dump --split-files %s/%s_temp%s.sra -O %s \n'%(path, gsm,i+1, path))
+            cat_file1 = cat_file1 + '%s/%s_temp%s_1.fastq '%(path, gsm,i+1)
+            cat_file2 = cat_file2 + '%s/%s_temp%s_2.fastq '%(path, gsm,i+1)
+        os.system('cat %s> %s/%s_R1.fastq \n'%(cat_file1, path,gsm))
+        os.system('cat %s> %s/%s_R2.fastq \n'%(cat_file2, path,gsm))
+        os.system('rm %s/%s_temp* \n'%(path, gsm))
+        os.system('gzip %s/%s_R1.fastq \n'%(path, gsm))
+        os.system('gzip %s/%s_R2.fastq \n'%(path, gsm))
+    return True
 
+def downloadMicroarrayData(gsm_html):
+    link_regexp = re.compile(r'href=.*[Cc][Ee][Ll].*>\(ftp\)')
+    link_infor = link_regexp.search(gsm_html)
+    if link_infor:
+        link = link_infor.group().replace('">(ftp)','').replace('href="','')
+        sys.stdout.write("Found ftp download link.")
+        cmd = "wget %s" % link
+        return cmd
+    else:
+        sys.stderr.write("ERROR: Failed to download find CEL files.\n")
+        sys.exit(5)
 
+def getLayType(srx_html,gsm):
+    lay_type = re.search('<div>Layout: <span>.{6}</span>',srx_html)
+    if lay_type:
+        lay_type = lay_type.group()
+        lay_type = lay_type[-13:-7]
+        sys.stderr.write(lay_type + "\n")
+        if lay_type != "SINGLE" and lay_type != "PAIRED":
+            lay_type = None
+            sys.stderr.write('ERROR: neither PAIRED nor SINGLE end sequencing: %s.\n'%gsm)
+            sys.exit(3)
+        else:
+            return lay_type
 
-def LinkPlusDownload(gsm, path):
-    """get link sequece type and return the path of sbatch file
-    """
-    os.system('echo %s'%gsm)
-    gsm_url = 'http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=%s'%gsm
-    gsm_handler = urllib.request.urlopen(gsm_url)
-    gsm_html = gsm_handler.read()
-    gsm_html = gsm_html.decode('utf-8')
-    # get the ftp location of SRX file and the SRX id
-    srx_regexp = re.compile('https://www.ncbi.nlm.nih.gov/sra\S*"')
-    srx_infor = srx_regexp.search(gsm_html)
+def getSRR(srx_html):
+    srr_regexp = re.compile('>SRR[0-9]*</a></td><td')
+    srr = srr_regexp.findall(srx_html)
+    srr = [i.lstrip(">").split("</a")[0] for i in srr]
+    return srr
+
+def getSrxHtml(gsm_html):
+    srx_infor = re.search('https://www.ncbi.nlm.nih.gov/sra\S*"',gsm_html)
     if srx_infor:
         srx = srx_infor.group().rstrip('"').lstrip('https://www.ncbi.nlm.nih.gov/sra?term=')
         # get the SRR id('>SRR1588518</a></td><td') and find the type of layout
         srx_url = 'http://www.ncbi.nlm.nih.gov/sra?term=%s'%srx
-        srx_handler = urllib.request.urlopen(srx_url)
-        srx_html = srx_handler.read()
-        srx_html = srx_html.decode('utf-8')
-        # find the layout type (<div>Layout: <span>SINGLE</span>)
-        lay_infor = re.compile('<div>Layout: <span>.{6}</span>')
-        lay_type = lay_infor.search(srx_html)
-        lay_type = lay_type.group()
-        lay_type = lay_type[-13:-7]
-        # get the srr id and download the sra files
-        srr_regexp = re.compile('>SRR[0-9]*</a></td><td')
-        srr = srr_regexp.findall(srx_html)
-        if lay_type == 'SINGLE':
-            fastq_file = '%s/%s.fastq'%(path,gsm)
-             # single.append(gsm)
-             # print >>SingleEnd_gsm, gsm
-        elif lay_type == 'PAIRED':
-            fastq_file = '%s/%s_R1.fastq,%s/%s_R2.fastq'%(path,gsm,path,gsm)
-        else:
-            print(('+++neither PAIRED nor SINGLE end sequencing: %s+++'%gsm))
-            return(None, None)
-        fastCmd = catfastq(path, gsm,srx_infor,srr,lay_type)
-        os.system(fastCmd)
+        srx_html = urllib.request.urlopen(srx_url).read().decode('utf-8')
+        return srx_html
     else:
-        print(('WARNING: no srx number for %s. Another type of data?'%gsm))
-        link_regexp = re.compile('href="ftp://ftp.ncbi.nlm.nih.gov/geo/samples/\S*\(ftp\)')
-        link_infor = link_regexp.search(gsm_html)
-        if link_infor:
-            link = link_infor.group().replace('">(ftp)','').replace('href="','')
-            print("Find ftp download link.")
-            ftpcmd = "wget %s" % link
-            os.system(ftpcmd)
-        else:
-            print("ERROR: Failed to download this GSM!")
+        return None
+
+def getGsmHtml(gsm):
+    gsm_url = 'http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=%s'%gsm
+    gsm_html = urllib.request.urlopen(gsm_url).read().decode('utf-8')
+    return gsm_html
 
 
-if __name__ == "__main__":
+def main():
     if len(sys.argv) < 2:
-        print("++download fastq data by GSM id:", file=sys.stderr)
+        print("++download data by GSM id:", file=sys.stderr)
         print("gsmid\tpath", file=sys.stderr)
         print("1\tgsmid", file=sys.stderr)
         print("2\toptional: the path to save fastq", file=sys.stderr)
         sys.exit(1)
     elif len(sys.argv) == 2:
-    	gsm = sys.argv[1]
-    	path = os.getcwd()
+        gsm = sys.argv[1]
+        path = os.getcwd()
     else:
-    	gsm = sys.argv[1]
-    	path = sys.argv[2]
-    LinkPlusDownload(gsm, path)
+        gsm = sys.argv[1]
+        path = sys.argv[2]
+
+
+    os.system('echo %s' % gsm)
+    gsm_html = getGsmHtml(gsm)
+    srx_html = getSrxHtml(gsm_html)
+    if srx_html:
+        SRR = getSRR(srx_html)
+        if SRR:
+            LayType = getLayType(srx_html,gsm)
+            GEO_status = downloadFastqFromGEO(path,gsm,SRR,LayType)
+            if GEO_status == False:
+                sys.stdout.write("WARNING: Could NOT download %s sra file from GEO. Trying EBI.\n" % gsm)
+                EBI_status = downloadFastqFromEBI(path,gsm,SRR,LayType)
+                if EBI_status == False:
+                    sys.stderr.write("ERROR: Could NOT download %s sra file from EBI. END QUERY.\n" % gsm)
+                    sys.exit(6)
+        else: 
+            sys.stderr.write("ERROR: Do not find SRR information for %s. Experiment data may not be public.\n" % gsm)
+            sys.exit(4)
+    else: # may not seq data
+        sys.stdout.write("WARNING: Do not find SRX information for %s. Microarray Data?\n" % gsm)
+        cmd = downloadMicroarrayData(gsm_html)
+        os.system(cmd)
+
+
+if __name__ == "__main__":
+    main()
+    
+
+
 
 
 
