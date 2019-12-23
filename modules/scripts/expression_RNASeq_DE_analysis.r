54793e58c24f69c242278496b991e9eacf3099c2
Xin Dong <dongxin9511@live.com>
2019-12-23 16:22:18 +0800

    fix bugs on batch removal

--- modules/scripts/expression_RNASeq_DE_analysis.r
+++ modules/scripts/expression_RNASeq_DE_analysis.r
@@ -34,19 +34,20 @@ opt = parse_args(opt_parser)
 
 profile_path=opt$input 
 result_path=opt$result
-treat = opt$treatname
-control = opt$controlname
+treatname = opt$treatname
+controlname = opt$controlname
 treatsample = strsplit(opt$treat,',')[[1]]
 controlsample = strsplit(opt$control,',')[[1]]
 
 rawcount=as.matrix(read.table(profile_path,header =T,sep="\t"))
 tran_gene=rawcount[,c(1,2)]
 rawcount=rawcount[,c(-1,-2)]
+rownames(rawcount) <- tran_gene[,1]
 mode(rawcount)<-"integer"
 design_matrix <- as.data.frame(c(treatsample,controlsample))
 colnames(design_matrix)="sample"
-design_matrix$condition <- c(rep(control,nrow(design_matrix)))
-design_matrix$condition[match(treatsample, as.character(design_matrix$sample))] <- treat
+design_matrix$condition <- c(rep(controlname,nrow(design_matrix)))
+design_matrix$condition[match(treatsample, as.character(design_matrix$sample))] <- treatname
 design_matrix$label_c <- c(rep(1,nrow(design_matrix)))
 design_matrix$label_c[match(treatsample,design_matrix$sample)] <- 2
 # print(design_matrix)
@@ -57,17 +58,17 @@ if(length(treatsample) >= 2 & length(controlsample) >= 2){
   rawcount = rawcount[,match(design_matrix$sample,colnames(rawcount))]
   sampleTable <- data.frame(sampleName = design_matrix$sample,  condition = design_matrix$condition)
   sampleTable$condition <- factor(sampleTable$condition)
-  sampleTable$condition <- relevel(sampleTable$condition, ref = control)
+  sampleTable$condition <- relevel(sampleTable$condition, ref = controlname)
 
   ddsFullCountTable <- DESeqDataSetFromMatrix(countData = rawcount, colData = sampleTable, design= ~ condition)
   dds <- DESeq(ddsFullCountTable)
+  # dds <- dds[rowSums(counts(dds)) > 0,]
   dat <- counts(dds, normalized=TRUE)
-  idx <- rowMeans(dat) > 1
+  idx <- rowMeans(dat) > 1 
   dat <- dat[idx,]
   mod <- model.matrix(~ design_matrix$label_c, colData(dds))
   mod0 <- model.matrix(~ 1, colData(dds))
-  n.sv <- num.sv(dat,mod,method="leek")
-  print(n.sv)
+  # n.sv <- num.sv(dat,mod,method="leek")
   svseq <- svaseq(dat, mod, mod0, n.sv=1)
   ddssva <- dds
   ddssva$SV1 <- svseq$sv[,1]
