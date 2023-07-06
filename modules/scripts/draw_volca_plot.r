a2a3049ee96c6c74879ef93448350548d91f0344
Xin Dong <xindong9511@gmail.com>
2023-07-06 23:23:47 +0800

    fix a format problem

--- modules/scripts/draw_volca_plot.r
+++ modules/scripts/draw_volca_plot.r
@@ -34,13 +34,11 @@ fdr_setting <- as.numeric(fdr_setting)
 # logFC_setting <- log2(FC_setting)
 
 # file_path="/Users/shixiaoying/Downloads/result_path.txt"
-file=read.table(input_path,sep = "\t", header = TRUE,
-                stringsAsFactors = FALSE,
-                quote = "", fill = TRUE)
+file <- read.table(input_path, sep = "\t", header = TRUE, stringsAsFactors = FALSE, quote = "", fill = TRUE)
 sigout <- subset(file, abs(logFC) > logFC_setting & adj.P.Val < fdr_setting)
-index=order(sigout$logFC)
+index <- order(sigout$logFC)
 if(length(index) > 20){
-sigout=sigout[c(index[1:10],rev(index)[1:10]),]}
+sigout <- sigout[c(index[1:10],rev(index)[1:10]),]}
 
 upR <- as.vector(unlist(sigout$symbol[which(sigout$logFC > 0)]))
 downR <- as.vector(unlist(sigout$symbol[which(sigout$logFC < 0)]))
@@ -50,8 +48,10 @@ volca <- ggplot(file,aes(logFC,-log10(adj.P.Val))) +
   scale_colour_manual("Significance", values = c("grey","red")) +
   labs(title = 'Volcano plot', x = "log2 Fold Change", y = "-log10 adj.P.Val")+
   geom_text_repel(data = sigout, aes(label = sigout$symbol), cex = 3,box.padding = unit(0.35, "lines"), point.padding = unit(0.3, "lines"))+
-  theme_bw()+geom_hline(aes(yintercept=-log10(fdr_setting)),linetype="dashed",color="grey",size=0.5)+
+  theme_bw()+
+  geom_hline(aes(yintercept=-log10(fdr_setting)),linetype="dashed",color="grey",size=0.5)+
   geom_vline(aes(xintercept=logFC_setting),linetype="dashed",color="grey",size=0.5)+
-  geom_vline(aes(xintercept=-logFC_setting),linetype="dashed",color="grey",size=0.5)+theme(axis.text.x = element_text(size = 10))+theme(axis.text.y = element_text(size = 10))
+  geom_vline(aes(xintercept=-logFC_setting),linetype="dashed",color="grey",size=0.5)+theme(axis.text.x = element_text(size = 10))+
+  theme(axis.text.y = element_text(size = 10))
 
 ggsave(result_path, volca, width = 7, height = 7)
